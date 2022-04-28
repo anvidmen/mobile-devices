c0f70456f2b422d25f3800f9ec76a87f7c00fdb6
Juan David Omen <anvidmen@gmail.com>
2022-04-28 12:52:00 +0200

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import { StyledHeader } from './styles'
+
+const Header = ({ title }) => <StyledHeader>{title}</StyledHeader>
+
+export default Header
+ 
---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import Colors from 'theme/Colors'
+
+export const StyledHeader = styled.header`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    font: normal normal normal 20px Helvetica Neue;
+    letter-spacing: 0px;
+    letter-spacing: 0px;
+    color: ${Colors.bgColor};
+    background: #f6f6f6 0% 0% no-repeat padding-box;
+    opacity: 1;
+    height: 3rem;
+    margin-top: 4.5rem;
+    padding-left: 1rem;
+`
