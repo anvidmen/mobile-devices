d551259dec9ba2028b4c4f81b567386d34ab257e
Juan David Omen <anvidmen@gmail.com>
2022-04-28 13:19:19 +0200

    implemented the card and the logic in view of mobile devices

--- src/views/MobileDevices/MobileDevices.jsx
+++ src/views/MobileDevices/MobileDevices.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
+import Body from 'components/Body/Body'
+import { Container } from './styles'
+import Card from 'views/MobileDevices/Card/Card'
+import retrieveMobileDevices from 'logic/retrieve-mobile-devices'
 
 const MobileDevices = () => {
-    return (
-        <>
-        </>
-    )
+  const [mobileDevices, setMobileDevices] = useState([])
+
+  useEffect(() => {
+    const getMobileDevices = async () => {
+      try {
+        const mobileDevices = await retrieveMobileDevices()
+        setMobileDevices(mobileDevices)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getMobileDevices()
+  }, [])
+
+  return (
+    <Body title="Mobile devices">
+      <Container>
+        {mobileDevices && mobileDevices.map(mobileDevice => (
+          <Card key={mobileDevice.id} mobileDevice={mobileDevice} />))
+        }
+      </Container>
+    </Body>
+  )
 }
 
 export default MobileDevices
