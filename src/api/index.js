8712fe0fbfdeda857c522509ebfef05d49da99bc
Juan David Omen <anvidmen@gmail.com>
2022-05-01 19:03:31 +0200

    added endpoint to get product details

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const baseURL = 'https://front-test-api.herokuapp.com'
 
 export const getMobileDevices = () => axios.get(`${baseURL}/api/product`)
+export const getProductDetail = id => axios.get(`${baseURL}/api/product/${id}`)
