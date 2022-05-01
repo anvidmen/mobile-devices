4e32b3ca1b5d0a606fc43bd31ccd80db8426119c
Juan David Omen <anvidmen@gmail.com>
2022-05-01 18:52:05 +0200

    modified styles of some Card properties

--- src/views/MobileDevices/Card/styles.js
+++ src/views/MobileDevices/Card/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Colors from 'theme/Colors'
+
 export const StyleCard = styled.div`
     display: flex;
     flex-direction: column;
@@ -10,31 +11,42 @@ export const StyleCard = styled.div`
     -webkit-border-radius: 10px;
     box-shadow: 1px 3px 6px 3px #00000039;
     overflow: hidden;
-    max-width: 16rem;
-    height: 26rem;
+    width: 17rem;
+    height: 23rem;
     margin: 1rem;
     padding: 1rem;
 `
 
 export const Image = styled.img`
-    width: 100%;
-    height: 100%;
+    position: relative;
+    overflow: hidden;
+    width: 10rem;
+    height: 12rem;
+
+    &:hover {
+      transform: scale(1.1);
+      transition: all 1s linear;
+    }
 `
 
 export const CardContent = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    flex-direction: column;
-    margin: 7px;
+    flex-direction: row;
+    transition: all 1s linear;
+    margin-top: 2rem;
+    width: 100%;
+    height:100%;
 `
 
 export const Title = styled.label`
     font: normal bolder normal 25px Helvetica Neue;
-    margin-bottom: 5px;
+    margin-bottom: 2rem;
 `
 
 export const TextContent = styled.label`
-    font: normal bold normal 16px Helvetica Neue;
-    margin: 2px;
+    font: ${props => props.price ? "normal bold normal 14px italic": "normal bold normal 16px italic"};
+    color: ${props => props.price? "red": "black"};
+    margin-top: 1rem
 `
