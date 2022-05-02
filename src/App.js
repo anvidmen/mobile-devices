0bc07fbaea02329507d226aa1e9dfbe9024db34e
Juan David Omen <anvidmen@gmail.com>
2022-05-02 18:06:15 +0200

    added shopping cart and product details paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Cart from 'views/Cart/Cart'
 import MobileDevices from 'views/MobileDevices/MobileDevices'
+import ProductDetail from 'views/ProductDetail/ProductDetail'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" caseSensitive={false} element={<MobileDevices/>} />
+        <Route path="/product-detail/:id" caseSensitive={false} element={<ProductDetail/>} />
+        <Route path='/shopping-cart' caseSensitive={false} element={<Cart/>} />
       </Routes>
     </Router>
   )
