29688449cb10859ecfe0113e9c8e35b07cb99f28
Juan David Omen <anvidmen@gmail.com>
2022-05-02 17:37:28 +0200

    wrapped the app with the state variable app Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import AppProvider from 'providers/AppProvider'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>
 )
 
